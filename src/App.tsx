704a3dbd3d77e581f196605d3f8551d57b1e8135
liuwei389511 <liuwei3895@gmail.com>
2025-05-20 19:54:17 +0800

    feat: 修复对话框夜间模式

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,21 @@ function App() {
     }
   };
 
+  const pro = new Promise((resolve, reject) => {
+    const innerpro = new Promise((r, reject) => {
+      setTimeout(() => {
+        r(1);
+      });
+      console.log(2);
+      r(3);
+    });
+    resolve(4);
+    innerpro.then((res) => console.log(res));
+    console.log("yideng");
+  });
+  pro.then((res) => console.log(res));
+  console.log("end");
+
   const sendMessage = async () => {
     setThink("思考中......");
     const apiUrl: string = "https://workertest.liuwei3895.workers.dev/graphql";
